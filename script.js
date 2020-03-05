7a055b493c3b616fc38848991180ed6e48cabeee
Alexander Bertilsson <alexander.bertilsson@outlook.com>
2020-03-05 12:55:07 +0100

    if success

--- script.js
+++ script.js
@@ -5,9 +5,14 @@
 function getKey(){
 var json = fetch('https://www.forverkliga.se/JavaScript/api/crud.php?requestKey')
   .then((response) => {
+    
     return response.json();
   })
   .then((data) => {
     console.log(data);
+    if(data.status == 'success') {
+        console.log('it was successful');
+    }
+
   });
 }
