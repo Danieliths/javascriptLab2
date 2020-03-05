849108ac812d94af56064626a6ee74d7a790619b
Daniel <daniel.andersson@iths.se>
2020-03-05 13:01:48 +0100

    working keysave

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 //    fetch('https://www.forverkliga.se/JavaScript/api/crud.php?requestKey').then((data) => {
 //        console.log(data)});
 //}
+var key = '';
 function getKey(){
 var json = fetch('https://www.forverkliga.se/JavaScript/api/crud.php?requestKey')
   .then((response) => {
@@ -11,8 +12,13 @@ var json = fetch('https://www.forverkliga.se/JavaScript/api/crud.php?requestKey'
   .then((data) => {
     console.log(data);
     if(data.status == 'success') {
-        console.log('it was successful');
+      console.log('it was successful');
+      key = data.key;
+      console.log('your key is: ' + key)
     }
 
   });
+}
+function addBook(){
+  console.log(key);
 }
