6a5e738c16fddbe2d8be9944b0d55ce19c4f194c
Daniel <daniel.andersson@iths.se>
2020-03-05 11:59:33 +0100

    basic js

---
+++ script.js
@@ -0,0 +1,13 @@
+//function getKey(){
+//    fetch('https://www.forverkliga.se/JavaScript/api/crud.php?requestKey').then((data) => {
+//        console.log(data)});
+//}
+function getKey(){
+var json = fetch('https://www.forverkliga.se/JavaScript/api/crud.php?requestKey')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
+}
