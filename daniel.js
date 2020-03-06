56e45a7410b644e614d07b969c2885074d1c066b
Daniel <daniel.andersson@iths.se>
2020-03-06 11:09:11 +0100

    cleanup daniel.js

--- daniel.js
+++ daniel.js
@@ -1,7 +1,7 @@
-var baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php';
-var addQuery = '&op=insert';
-var showQuery = '&op=select';
-var keyQuery = '?key=';
+const baseUrl = 'https://www.forverkliga.se/JavaScript/api/crud.php';
+const addQuery = '&op=insert';
+const showQuery = '&op=select';
+const keyQuery = '?key=';
 var numberOfRetrys = 0;
 
 function message(succsess, message){
@@ -19,29 +19,28 @@ function message(succsess, message){
 function showBooksMenu(){
     var consoleDiv = document.getElementById('console');
     consoleDiv.innerHTML = '<label>Input book id </label>  ' +
-    '<input type="text" id="showBookId" name="test3"><br>' +
+    '<input type="text" id="showBookId"><br>' +
     '<button id="bajskaka" onclick="showBooks()">Confirm</button>';
 }
 
 function addBookMenu(){
     var consoleDiv = document.getElementById('console');
     consoleDiv.innerHTML = '<label>Input title </label>  ' +
-    '<input type="text" id="bookTitle" name="test"><br>' +
+    '<input type="text" id="bookTitle"><br>' +
     '<label>Input author </label>  ' +
-    '<input type="text" id="bookAuthor" name="test2"><br>' +
+    '<input type="text" id="bookAuthor"><br>' +
     '<button id="addBook" onclick="addBook()">Confirm</button>';
 }
 
 function addBook(){
-    var title = document.getElementById('bookTitle').value;
-    var author = document.getElementById('bookAuthor').value;
+    let title = document.getElementById('bookTitle').value;
+    let author = document.getElementById('bookAuthor').value;
         fetch(baseUrl + keyQuery + key + addQuery + '&title=' + title + '&author=' + author)
         .then((response) => {
         return response.json();
         })
         .then((data) => {
         if(data.status == 'success'  || numberOfRetrys > 2) {
-            console.log(data);
             numberOfRetrys = 0;
             message(data.status, data.id);
         } 
@@ -51,40 +50,36 @@ function addBook(){
         }
         });
 };
-function showBooks(){
+function fetchLibary(){
         fetch(baseUrl + keyQuery + key + showQuery)
         .then((response) => {
         return response.json();
         })
         .then((data) => {
         if(data.status == 'success'  || numberOfRetrys > 2) {
-            console.log(data.data);
             numberOfRetrys = 0;
             message(data.status, data.id);
-            var showBooksText = '';
-            for (let index = 0; index < data.data.length; index++) {
-                const element = data.data[index];
-                showBooksText = showBooksText + 
-                '<br><label>Id: ' + element.id + '</label> <br>' +
-                '<label>title: ' + element.title + '</label> <br>' +
-                '<label>author: ' + element.author + '</label> <br>' +
-                '<label>updated: ' + element.updated + '</label>'
+            if(data.status == 'success'){ 
+                displayBooks(data);
             }
-            var consoleDiv = document.getElementById('console');
-            consoleDiv.innerHTML = showBooksText;
         }
         else {
             numberOfRetrys++;
-            showBooks();
+            fetchLibary();
         }
     });
 };
-function displayBooks(array){
-    var consoleDiv = document.getElementById('console');
-    array.forEach(element => {
-        consoleDiv.innerHTML = '<label>Id: ' + element.id + '</label> <br>'
-    });
 
-}
-// add book
-// show books
+function displayBooks(array){
+    let showBooksText = '';
+    for (let index = 0; index < array.data.length; index++) {
+        const element = array.data[index];
+        showBooksText = showBooksText + 
+        '<br><label>Id: ' + element.id + '</label> <br>' +
+        '<label>title: ' + element.title + '</label> <br>' +
+        '<label>author: ' + element.author + '</label> <br>' +
+        '<label>updated: ' + element.updated + '</label><br><br>'
+    }
+    let consoleDiv = document.getElementById('console');
+    consoleDiv.innerHTML = showBooksText;
+};
